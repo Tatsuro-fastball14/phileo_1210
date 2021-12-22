bbe9f74fdb68d8cda99c16f22687d88dd29ee65f
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-12-22 23:24:24 +0900

    show_5
    show_5

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -43,8 +43,10 @@ class CooksController < ApplicationController
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-  end
+    username == 'admin' && password == '2222'
     end
+
+  end
+      
 end
 
