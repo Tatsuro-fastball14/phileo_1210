7dcd510351084fed12482347bab26b5768d92d8a
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2022-02-10 22:16:35 +0900

    v-for fix3
    v-for fix3

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,33 +1,85 @@
-<template>
-  <div id="app">
-    <p>{{ message }}</p>
-    <p>{{ cooks }}</p>
-    <ul>
-        <li v-for ="cook in cooks">{{ cook }}</li>
-    </ul>
+//vuetest.js
+// import Vue from "Vue"
+
+const exp = new "Vue"({
+  el: '#test',
+  data:{
+        cook:[
+          "title"
+          "store"
+          "cooksentence"
+          "address"
+          "phone_number"
+          "open_day"
+          "holiday_day"
+          "regular_hoilday"
+        ]
+   }
+})
+
+  //vuetest.js
+  const exp =new Vue({
+        el: "#text",
+        date:{
+          cook:[
+            {information: "title"},
+            {information: "store"},
+            {information: "cooksentence"},
+            {information: "address"},
+            {information: "phone_number"},
+            {information: "open_day"},
+            {information: "holiday_day"},
+            {information: "regular_hoilday"},   
+          ]                   
+        },          
+   })
 
 
-  </div>
-</template>
 
-<script>
-export default {
-  data: function () {
 
-    return {    
-      message: "Hello Vue!",
-      cooks: [{store: "awamori", lat: 26.2155658, lng: 127.6691134}],
-      {store: "awamori", lat: 1222.2222, lng: 3737},
-      {store: "awamori", lat: 1222.2222, lng: 3737}]
 
-    }
-  }
-}
-</script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
+
+
+
+
+
+
+
+
+
+
+
+// <template>
+  // <div id="app">
+  //   <p>{{ message }}</p>
+  //   <p>{{ cooks }}</p>
+  //   <ul>
+        // <li v-for ="cook in cooks">{{ cook }}</li>
+  //   </ul>
+
+
+//   </div>
+// </template>
+
+// <script>
+// export default {
+//   data: function () {
+
+//     return {    
+//       message: "Hello Vue!",
+//       cooks: [{store: "awamori", lat: 26.2155658, lng: 127.6691134}],
+//       {store: "awamori", lat: 1222.2222, lng: 3737},
+//       {store: "awamori", lat: 1222.2222, lng: 3737}]
+
+//     }
+//   }
+// }
+// </script>
+
+// <style scoped>
+// p {
+//   font-size: 2em;
+//   text-align: center;
+// }
+// </style>
