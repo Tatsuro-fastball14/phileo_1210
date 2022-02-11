be3d0bc947c66b191d9a85cc83b896281696c9f7
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2022-02-12 06:33:15 +0900

    v-for fix_5
    v-for fix_5

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,56 +1,39 @@
-//vuetest.js
-// import Vue from "Vue"
-
-  const exp = new "Vue"({
-      el: '#test',
-      data:{
-        cook:[
-          "title"
-          "store"
-          "cooksentence"
-          "address"
-          "phone_number"
-          "open_day"
-          "holiday_day"
-          "regular_hoilday"
-        ]
-      }
-  })
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <p>@{{ cooks }}</p>
+    <ul>
+        <li v-for ="cook in cooks">@{{ cooks }}</li>
+    </ul>
 
 
-    <div id="test">
-        <ul>
-            <li v-for="cooks in cook">
-              @{{cooks}}
-            </li>
-        </ul>
-    </div>
-    <script src="{{asset('js/vuetest.js')}}"></script>
+  </div>
+</template>
 
+<script>
+export default {
+  el: "#app",
+  data: function () {
 
-  //vuetest.js
-  const exp =new Vue({
-        el: "#test",
-        date:{
-          cook:[
-            {information: "title"},
-            {information: "store"},
-            {information: "cooksentence"},
-            {information: "address"},
-            {information: "phone_number"},
-            {information: "open_day"},
-            {information: "holiday_day"},
-            {information: "regular_hoilday"},   
-          ]                   
-        },          
-   })
+    return {    
+      message: "Hello Vue!",
+      cooks: [
+        {store: "awamori", lat: 26.2155658, lng: 127.6691134},
+        {store: "awamori", lat: 1222.2222, lng: 3737},
+        {store: "awamori", lat: 1222.2222, lng: 3737},
+      ]
 
+    },
+  },
+}
+</script>
 
-
-
-
-
-
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
 
 
 
