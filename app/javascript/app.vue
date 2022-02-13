52c054b25ad8790cab529d30b45deaa649935309
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2022-02-13 22:56:45 +0900

    props
    props

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -37,6 +37,49 @@ p {
 
 
 
+export default{
+  props: {
+    myLatLng: {
+      type: object,
+      required: ture,
+    },
+    zoom: {
+      type: Number,
+      required: ture,
+    },
+  },
+  mounted(){
+    if (!window.mapLoadStarted) {
+      window.mapLoadStarted = true;
+      let script = document.createElement('script');
+      script.src =
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixta.jp%2Fillustration%2F12767983&psig=AOvVaw0JcQTIocWdS2K6xFdU8EYV&ust=1638779689532000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjvx5ygzPQCFQAAAAAdAAAAABAO';
+        script.async = true;
+        document.head.appendChild(script);
+    }
+
+    window.initMap =() => {
+      window.mapLoaded = true;
+    };
+    
+    let timer = setInterval(() => {
+      if (window. mapLoaded) {
+        clearInterval(timer);
+        const map = new
+    window.google.maps.Map(this.$refs.map, {
+          center: this.myLatLng,
+          zoom: 15,
+          mapTypeId: 'roadmap'
+
+
+    });
+    new window.google.maps.Marker({ position:
+    this.myLatLng, map });
+      }
+    },500);
+  },
+};
+
 
 
 
