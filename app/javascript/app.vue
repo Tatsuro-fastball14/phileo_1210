0b4edd6252fe7ce95bf758d4e83538b7af5efb36
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2022-02-18 06:56:01 +0900

    propsを使ってマップの位置を変更_3
    propsを使ってマップの位置を変更_3

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
     <p>@{{ cook.id }}</p>
     <ul>
         <li v-for ="cook in cooks">@{{ cook.id }}</li>
@@ -14,29 +13,14 @@
 export default {
   el: "#app",
   data: function () {
-
-    return {    
-      message: "Hello Vue!",
+    return {       
       cooks: [
         {store: "awamori", lat: 26.2155658, lng: 127.6691134},
         {store: "awamori", lat: 1222.2222, lng: 3737},
         {store: "awamori", lat: 1222.2222, lng: 3737},
       ]
+    }
+  }
 
-    },
-  },
+
 }
-</script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
-
-
-
-
-
-
