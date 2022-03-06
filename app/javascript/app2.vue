2e2f0e6732b42447b3f5ccac62f1de8ea950070f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-03-06 18:50:26 +0900

    template_fix
    template_fix

--- app/javascript/app2.vue
+++ app/javascript/app2.vue
@@ -60,15 +60,3 @@ export default {
 </script>
 
 
-<template>
-  <h1>Google Map</h1>
-  <div ref="map" style="height: 500px; width: 800px"></div>
-</template>
-
-<script>
-export default {
-  mounted() {
-    this.$showMap(this.$refs.map, { lat: 34.344, lng: 136.036 });
-  },
-};
-</script>
---
+++ app/javascript/app3.tempalte.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Google Map</h1>
+  <div ref="map" style="height: 500px; width: 800px"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$showMap(this.$refs.map, { lat: 34.344, lng: 136.036 });
+  },
+};
+</script>
