e36a14eddb0982b268ee3218c3c64d8a67bd0d64
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-04-02 20:09:02 +0900

    vue.jsを読み込み
    vue.jsを読み込み

---
+++ app/javascript/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    'test'    
+    <ul>
+        <li v-for ="cook in cooks">
+          {{ cook.id }}
+        </li>
+    </ul>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  el: "#app",
+  data: function () {
+    return {
+      cook:'cook.id',
+      cooks: [
+        {store: "awamori", lat: 26.2155658, lng: 127.6691134},
+        {store: "awamori", lat: 1222.2222, lng: 3737},
+        {store: "awamori", lat: 1222.2222, lng: 3737},
+      ]
+    }
+  }
+
+
+}
+</script>
+
