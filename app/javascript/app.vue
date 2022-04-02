ffa1569ccc7dfd0d4f838fbb89160a12ec8fceb0
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-04-02 21:55:06 +0900

    app.vue
    app.vue 修正

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,31 +1,47 @@
 <template>
-  <div id="app">
-    'test'    
-    <ul>
-        <li v-for ="cook in cooks">
-          {{ cook.id }}
-        </li>
-    </ul>
-
-
+  <div>
+     <h1>Google Map</h1>
+     <div ref="map" style="height: 500px ; width: 800px"></div>
   </div>
 </template>
 
 <script>
 export default {
-  el: "#app",
   data: function () {
     return {
-      cook:'cook.id',
       cooks: [
         {store: "awamori", lat: 26.2155658, lng: 127.6691134},
-        {store: "awamori", lat: 1222.2222, lng: 3737},
-        {store: "awamori", lat: 1222.2222, lng: 3737},
+        {store: "awamori", lat: 26.176827006031758, lng: 127.6751037628797},
+        {store: "awamori", lat: 26.18668612463146, lng: 127.66806564613702}
       ]
     }
-  }
+    },
+  mounted() {
+    if (!window.mapLoadStarted) {
+      window.mapLoadStarted = true;
+      let script = document.createElement('script');
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAc8ucfbF9aY5Jn9VehhJZ852fopENuQTc&callback=initMap';
+      script.async = true;
+      document.head.appendChild(script);
+    }
 
+ window.initMap = () => {
+      window.mapLoaded = true;
+    };
 
-}
+let timer = setInterval(() => {
+      if (window.mapLoaded) {
+        clearInterval(timer);
+        const map = new window.google.maps.Map(this.$refs.map, {
+          center: {lat: this.cooks[0].lat, lng: this.cooks[0].lng},
+          zoom: 15,
+        });
+        this.cooks.forEach(cook => {
+          new window.google.maps.Marker({position: {lat: cook.lat, lng: cook.lng}, map});
+        })
+      }
+    }, 500);
+  },
+};
+
 </script>
-
