8251d077a493774a3ce2647c8d43632de04c0d46
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-05-05 20:11:12 +0900

    差分確認
    差分確認

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,11 @@
 <template>
-
   <div>
     <h1>Google Map</h1>
     <div ref="map" style="height: 500px; width: 800px"></div>
-    
   </div>
-  
 </template>
 
 <script>
-import axios from 'axios'
 export default {
   data: function () {
     return {
@@ -19,20 +15,9 @@ export default {
         {store: "awamori", lat: 26.18668612463146, lng: 127.66806564613702},
       ]
     }
-  
   },
-
-  methods: {
-    fetchCooks() {
-      return axios.get('http://localhost:3000/cooks').then(res => {
-        console.log(res.data)
-        this.cooks = res.data;    
-      })
-    }
-  },
+ 
   mounted() {
-   
-    this.fetchCooks()
 
     if (!window.mapLoadStarted) {
       window.mapLoadStarted = true;
@@ -40,34 +25,26 @@ export default {
       script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAc8ucfbF9aY5Jn9VehhJZ852fopENuQTc&callback=initMap';
       script.async = true;
       document.head.appendChild(script);
-      window.initMap = () => {
-      window.mapLoaded = true;
-      }
     }
-  
+
+    window.initMap = () => {
+      window.mapLoaded = true;
+    };
+
     let timer = setInterval(() => {
       if (window.mapLoaded) {
         clearInterval(timer);
-        console.log{parsefloat(this.cooks[0].lat)}
         const map = new window.google.maps.Map(this.$refs.map, {
-          center: {lat: this.cooks[0].lat, lng: this.cooks[0].lng },
+          center: {lat: this.cooks[0].lat, lng: this.cooks[0].lng},
           zoom: 15,
         });
-          this.cooks.forEach(cook => {
-            new window.google.maps.Marker({position: {lat: cook.lat, lng: cook.lng}, map});
-            const marker = new google.maps.Marker({});
-            var infowindow = new google.maps.InfoWindow({
-              content: '関西空港',
-              position: {lat:cook.lat,lng:cook.lng}
-            });
-            infowindow.open(map);
+        this.cooks.forEach(cook => {
+          new window.google.maps.Marker({position: {lat: cook.lat, lng: cook.lng}, map});
         })
       }
     }, 500);
-  }, 
+  },
 };
+
 </script>
 
-
-
-  
