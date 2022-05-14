ad670666bb83e9a9e62c45e48f1581942b6fb578
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-05-14 23:26:08 +0900

    コメントアウト
    コメントアウトを行った

--- app/models/map.rb
+++ app/models/map.rb
@@ -3,10 +3,10 @@ class Map < ApplicationRecord
 
   private
   def geocode
-    uri = uri.escape(http://maps.google.com/maps/api/geocode/json?address"+self.address.gsubkey=AIzaSyAc8ucfbF9aY5Jn9VehhJZ852fopENuQT)
-    res = HTTP.get(uri).to_s
-    response = json.parse(res)
-    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    # uri = uri.escape(http://maps.google.com/maps/api/geocode/json?address"+self.address.gsubkey=AIzaSyAc8ucfbF9aY5Jn9VehhJZ852fopENuQT)
+    # res = HTTP.get(uri).to_s
+    # response = json.parse(res)
+    # self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+    # self.longitude = response["results"][0]["geometry"]["location"]["lng"]
   end
 end
