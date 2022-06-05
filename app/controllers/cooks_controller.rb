2d6b3ce62c8f3ef707899f31a88a79460a64d076
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-06-05 22:56:01 +0900

    edit,update,destroy
    edit,update,destroy Actionの追加

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,6 +1,8 @@
 class CooksController < ApplicationController
   before_action :search_cook, only: [:index, :search]
   before_action :basic_auth, only: [:new]
+  before_action :set_cook, only: [:edit, :show,:update,:destroy]
+
 
   def index
     @cooks = Cook.all
@@ -18,6 +20,25 @@ class CooksController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    
+    if@cook.update(cooks_params)
+        redirect_to cook_path
+    else
+      render :edit
+    end 
+  end
+
+  def destroy
+  
+    @cook.destroy
+    redirect_to  root_path
   end
 
   def show
@@ -46,5 +67,9 @@ class CooksController < ApplicationController
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '1414'
     end
+  end
+
+  def set_cook
+    @cook = Cook.find(params[:id])
   end
 end
