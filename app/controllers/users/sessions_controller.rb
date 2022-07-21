a2cdaa1dabebab2cf39002a48b36ef6a46ff9c4a
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-07-21 16:05:15 +0900

    login
    login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,10 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    @cook = Cook.last
+    super
+  end
 
   # POST /resource/sign_in
   # def create
@@ -18,7 +19,10 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  def after_sign_in_path_for(resource)
+    cook_path(7)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
