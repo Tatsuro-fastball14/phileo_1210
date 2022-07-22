d25cab95909181162538f63e6d9c39f95aabd239
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-07-22 21:25:12 +0900

    フレンドリーフォワーディングを実装_1
    フレンドリーフォワーディングを実装_1

--- app/helpers/cook_helper.rb
+++ app/helpers/cook_helper.rb
@@ -1,13 +1,2 @@
 module CookHelper
-
-  def redirect_back_or(default)
-    redirect_to(session[:forwarding_url] || default)
-    session.delete(:forwarding_url)
- end
-
-
- def store_location
-    session[:forwarding_url] = request.original_url if request.get?
- end
-
 end
