38970ea843fd62371cc1831d3c744964984bfe45
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-02 21:30:28 +0900

    引数
    引数

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,7 +26,8 @@ class Users::SessionsController < Devise::SessionsController
 
   
   def after_sign_in_path_for(resource)
-      stored_location_for(resource_or_scope) || super
+      stored_location_for(resource)
+     
   end
 
   # If you have extra params to permit, append them to the sanitizer.
