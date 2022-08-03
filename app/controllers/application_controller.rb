4e1c23a09582598529bfd23401aed611d023421c
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-04 08:27:39 +0900

    pay.jp
    pay.jp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     
       
 
-  end
+ 
 
   def store_user_location!
      
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
-end
+ require 'payjp'
+
+  def purchase
+    Payjp.api_key = "秘密鍵"
+    Payjp::Charge.create(
+      amount: 809, # 決済する値段
+      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+      currency: 'jpy'
+    )
+  end
