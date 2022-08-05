ac33de21850e8ea15e2a5ba247ee35ddfc100341
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-05 22:57:45 +0900

    <%= f.label :number,  "カード番号" %>
     <%= f.label :number,  "カード番号" %>

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,8 @@ Rails.application.routes.draw do
  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cooks do 
+  resources :cooks
   resources :orders
-
-  end
+ 
 
 end
