5267ba8f54858c477f8adae7ad0cac5e1021123c
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-12 08:02:47 +0900

    post先を修正
    post先を修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,16 @@ Rails.application.routes.draw do
   get 'members/new'
   post 'orders/index'
   
-  
   devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions'   
-} 
-
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
 
   root to: "places#index"
  
- 
-  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cooks
-  resources :orders,only: [:show, :create,:index] do
+  resources :orders, only: [:show, :create, :index] do
     collection do 
       post 'order'
     end
