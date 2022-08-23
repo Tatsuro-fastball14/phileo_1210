adc2f686419d001b28ffe91bbb548ded8a803d48
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-24 07:03:13 +0900

    js
    js

---
+++ app/assets/config/pay.js
@@ -0,0 +1,1 @@
+const elements = payjp.elements()
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,6 +29,15 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     )
     redirect_to root_path, notice: '登録が完了しました'
+
+    Payjp.api_key = "sk_test_387e29ac1993016a509c7ae9"
+    Payjp::Charge.create(
+      amount: 3000, # 決済する値段
+      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
+      currency: 'jpy'
+    )
+    redirect_to root_path, notice: '登録が完了しました'
+  
   end
 
   def pay
