6ded398ff89169105984edef55a25c88b6dd86ba
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-27 07:06:15 +0900

    JS実装
    JS実装

--- app/assets/config/card.js
+++ app/assets/config/card.js
@@ -1,6 +1,30 @@
-cardElement.on('change', (e) => {
-  document.querySelector('#brand').innerText = e.brand
-  document.querySelector('#empty').innerText = e.empty
-  document.querySelector('#complete').innerText = e.complete
-  document.querySelector('#error').innerText = JSON.stringify(e.error)
-})
+(function() {
+  if (!$('#regist_card')[0]) return false;
+  
+  Payjp.setPublicKey("テスト用の公開鍵");
+  
+  ("#regist_card").on("click", function(e) {
+    e.preventDefault();
+    var card = {
+        number: $("#card_number_form").val(),
+        exp_month: $("#exp_month_form").val(),
+        exp_year: $("#exp_year_form").val(),
+        cvc: $("#cvc_form").val()
+    };
+
+    Payjp.createToken(card, function(status, response) {
+      if (status === 200) {
+        ("#card_number_form").removeAttr("name");
+        ("#exp_month_form").removeAttr("name");
+        ("#exp_year_form").removeAttr("name");
+        ("#cvc_form").removeAttr("name");
+        var token = response.id;
+        ("#card_token").append(`<input type="hidden" name="card_token" value=${token}>`)
+        ("#card_form").get(0).submit();
+      } else {
+        alert("カード情報が正しくありません。");
+        ("#regist_card").prop('disabled', false);
+      }
+    });
+  });
+});
