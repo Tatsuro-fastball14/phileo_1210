d5195c1ef63f08eb701e11e42e120b88c9b1dc65
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-08-30 07:05:33 +0900

    修正
    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,12 +6,11 @@ class OrdersController < ApplicationController
   end
 
   def new
-  @order = Order.new
-    card = Card.where(user_id: current_user.id)
+      @order = Order.new
+      card = Card.where(user_id: current_user.id)
   end
-
   def create
-      @order = Order.new(order_params)
+        @order = Order.new(order_params)
      
     if @order.valid?
       @order.save
@@ -21,43 +20,25 @@ class OrdersController < ApplicationController
     end
   end
 
-   require 'payjp'
-
-
-   
+  require 'payjp'
+ 
   def order
-   var payjp = Payjp('pk_test_0383a1b8f91e8a6e3ea0e2a9')
+    payjp.api_key = '秘密鍵'
     Payjp::Charge.create(
       amount: 400, # 決済する値段
       card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
       currency: 'jpy'
     )
     redirect_to root_path, notice: '登録が完了しました'
-
-    var payjp = Payjp('pk_test_0383a1b8f91e8a6e3ea0e2a9')
-    Payjp::Charge.create(
-      amount: 3000, # 決済する値段
-      card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
-      currency: 'jpy'
-    )
-    redirect_to root_path, notice: '登録が完了しました'
-    
-
-
-    
-  
   end
 
-  def pay
-   
+  def pay 
   end
   
-
-  private
+ private
 
   def order_params
     params.require(:order).permit(:price)
   end
- 
 end
   
