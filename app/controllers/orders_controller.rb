af8afceec76f8ff9704750c78ae149e528313ba5
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-09-08 23:19:25 +0900

    修正
    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,47 +1,47 @@
 class OrdersController < ApplicationController
 
   def index
-      @order = Order.new
-     
+     @order = Order.new
+
   end
 
   def new
-      @order = Order.new
-      card = Card.where(user_id: current_user.id)
+    @order = Order.new
+    card = Card.where(user_id: current_user.id)
   end
+
   def create
-      @order = Order.new(order_params)
-        Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
-        Payjp::Customer.create(
-        description: 'test'         
-      )
-     
-  if  @order.valid?
-      @order.save
-      return redirect_to root_path
-    else
-      render 'index'
-    end
+     Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
+      customer = Payjp::Customer.create(
+      card: params['payjp-token']
+    )
+    Payjp::Subscription.create(
+      plan: 'getugaku400',
+      customer: customer.id
+    ) 
+ 
+
+
+
+
+   return redirect_to root_path
+    # TODO: payjpで作成したcustomer.idをアプリのDBに保存しておく必要がある。
+    # そうすることで、userとpayjp側のcustomerを判定できるようになる
+    # 判定できるようになると、Payjp::Customer.retrieve('カスタマーID')でpayjpの顧客情報を取得できるようになる
+
+    return redirect_to root_path
+
+
+
+
+
+
+
   end
-  
-    
-  def order
-   
-  end
-   
-  def pay 
-      require 'payjp'
-        Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
-        Payjp::Subscription.create(
-        plan: 'pln_9589006d14aad86aafeceac06b60',
-        ustomer: 'cus_4df4b5ed720933f4fb9e28857517'
-      )   
-  end
-  
- private
+  private
 
   def order_params
-    params.require(:order).permit(:price)
+    params.require(:order).permit(:price,:customer.id)
   end
 end
   
