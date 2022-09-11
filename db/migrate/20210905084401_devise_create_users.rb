b84d9b662e59104936c348de4d0c5a3cdaa73be7
yusukexyusuke <yusukexyusuke@gmail.com>
2022-09-11 11:36:44 +0900

    migrationの整理

--- db/migrate/20210905084401_devise_create_users.rb
+++ db/migrate/20210905084401_devise_create_users.rb
@@ -7,7 +7,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :customer_id
-      
 
       ## Recoverable
       t.string   :reset_password_token
