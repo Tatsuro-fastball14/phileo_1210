36ccdda1fdab382f78004e288e8c8e75abdc3998
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-09-18 22:08:34 +0900

    インデント、顧客（create)修正
    インデント、顧客（create)修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,18 +9,18 @@ class OrdersController < ApplicationController
   end
 
   def create
-     Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
+      Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
       customer = Payjp::Customer.create( 
         description: '登録テスト',        
         card: params['payjp_token'],
         metadata: {user_id: current_user.id}
       )
-        current_user.update:(customer_id: customer.id)
-          Payjp::Subscription.create(
-            plan: 'getugaku400',
-            customer: customer.id     
-          )
-            redirect_to action: "show"    
+      current_user.update(customer_id: customer.id)
+        Payjp::Subscription.create(
+          plan: 'getugaku400',
+          customer: customer.id     
+        )
+        redirect_to orders_path
   end
 
  
