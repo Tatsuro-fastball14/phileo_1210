94c70b0fe1be58952affc61caaf66b7ec2ac0a9f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-09-27 04:30:20 +0900

    修正
    修正０９２７

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,28 +9,21 @@ class OrdersController < ApplicationController
   end
 
   def create
-      Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
-      customer = Payjp::Customer.create( 
-        description: '登録テスト',        
-        card: params['payjp_token'],
-        metadata: {user_id: current_user.id}
-      )
-      current_user.update(customer_id: customer.id)
-      Payjp::Subscription.create(
+    Payjp.api_key = 'sk_test_387e29ac1993016a509c7ae9'
+    customer = Payjp::Customer.create(
+      description: '登録テスト',
+      card: params['payjp_token'],
+      metadata: {user_id: current_user.id}
+    )
+    current_user.update(customer_id: customer.id)
+    Payjp::Subscription.create(
       plan: 'getugaku400',
-      customer: customer.id     
-      )
-      def after_sign_in_path_for(resource)
-        if  resource.subscriber?    
-          stored_location_for(resource)
-        else     
-          orders_url       
-        end
-      end
+      customer: customer.id
+     )
+
+     redirect_to stored_location_for(current_user)
   end
 
- 
-  
   private
 
   def order_params
