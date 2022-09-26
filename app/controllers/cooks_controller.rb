3efd66c14729b60dd94d376f9e8a2c204201085b
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-09-27 04:57:40 +0900

    修正
    修正

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -41,7 +41,7 @@ class CooksController < ApplicationController
   end
 
   def show
-    binding.pry
+   binding.pry
     redirect_to orders_path unless current_user.subscriber?
     @cook = Cook.find(params[:id])
   end
