5a29bdb8ea12027b10e80fe54271d4e07e8befbc
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-09-29 18:15:24 +0900

    Railsで管理者権限（admin）を設定する
    Railsで管理者権限（admin）を設定する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_122856) do
+ActiveRecord::Schema.define(version: 2022_09_29_090007) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -71,8 +71,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_122856) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.boolean "admin"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
