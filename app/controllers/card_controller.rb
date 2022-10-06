82dccbde56e7f801ae6ed6a0c806b2d45c55acfb
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-10-07 07:07:46 +0900

    修正４
    修正４

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,29 +2,24 @@ class CardController < ApplicationController
   require "payjp"
   before_action :set_card
 
-  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
-    card = Card.where(user_id: current_user.id).first
-    redirect_to action: "index" if card.present?
-  end
-
 def new
-  card = Card.where(user_id: current_user.id)
-  redirect_to action: "show" if card.exists?
-  @card = Card.new(
-        user_id: current_user.id,
-        customer_id: customer.id,
-        card_id: customer.default_card
-  )
+    card = Card.where(user_id: current_user.id)
+      redirect_to action: "show" if card.exists?
+    @card = Card.new(
+      user_id: current_user.id,
+      customer_id: customer.id,
+      card_id: customer.default_card
+    )
 end
 
 def show
-  card = Card.find_by(user_id: current_user.id)
+    card = Card.find_by(user_id: current_user.id)
   if card.blank?
-    redirect_to action: "new" 
+      redirect_to action: "new" 
   else
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    customer = Payjp::Customer.retrieve(card.customer_id)
-    @default_card_information = customer.cards.retrieve(card.card_id)
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
   end
 end
 
