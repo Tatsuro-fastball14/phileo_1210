6b592317c61ad4fa66cf126bd17ec4aabfddd4fd
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-10-06 18:26:45 +0900

    完了
    完了

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    :sessions => 'admins/sessions'
+    
   } 
 
   root to: "places#index"
