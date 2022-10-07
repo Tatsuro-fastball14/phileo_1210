f82bc4a2ad7bd8f171a0f23bdb88090361f061e5
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-10-07 22:33:11 +0900

    修正５
    修正５

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -29,6 +29,11 @@ class Users::SessionsController < Devise::SessionsController
       end  
   end
 
+  protected
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
