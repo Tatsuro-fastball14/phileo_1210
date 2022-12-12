37b51b7e8e20d22ba7300c40f9c020bcc39b3c72
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-13 07:06:46 +0900

    DBに保存したい
    DBに保存したい

--- app/controllers/umarepos_controller.rb
+++ app/controllers/umarepos_controller.rb
@@ -4,4 +4,27 @@ class UmareposController < ApplicationController
   def new
     @umarepo = Umarepo.new
   end
+
+  def create
+     @umarepo = Umarepo.new(umarepos_params)
+    if @umarepo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+
+
+
+   def umarepos_params
+    params.require(:umarepo).permit(:title, :coment, images: [])
+  end
+
+
+
+
+
+
 end
