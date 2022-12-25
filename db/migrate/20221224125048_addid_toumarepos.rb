136895c72b89d8ca6da4ab394df8359563aef331
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-26 06:38:17 +0900

    class名（モデル）
    class名（モデル）

--- db/migrate/20221224125048_addid_toumarepos.rb
+++ db/migrate/20221224125048_addid_toumarepos.rb
@@ -1,4 +1,4 @@
-class AddidToumarepos < ActiveRecord::Migration[6.0]
+class AddIdToUmarepos < ActiveRecord::Migration[6.0]
   def change
      add_column :umarepos, :id, :bigint,  default: false
   end
