9c4511a3929752151fd67f933e53cc30aaf3b64f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2022-12-30 21:08:39 +0900

    each
    ech

--- app/controllers/umarepos_controller.rb
+++ app/controllers/umarepos_controller.rb
@@ -3,11 +3,13 @@ class UmareposController < ApplicationController
 
   def new
     @umarepos = Umarepo.new
+      binding.pry
    
   end
 
   def create
      @umarepos = Umarepo.new(umarepos_params)
+     binding.pry
      
     if @umarepos.save!
       redirect_to root_path
@@ -21,7 +23,7 @@ class UmareposController < ApplicationController
 
 
    def umarepos_params
-    params.require(:umarepo).permit(:title, :coment, images: [])
+    params.require(:umarepo).permit(:title, :comment, images: [])
   end
 
 
