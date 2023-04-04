97e1b78b798b645e2a893827187ceb6f1621e6f2
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-04-04 22:57:43 +0900

    修正
    修正

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cooks  do
-    resources :umarepos 
+    resource :umarepos 
   end
 
 
