f18f0d0c05b9a92ebf7b70553e1a67c2efb260b3
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-04-28 07:14:52 +0900

    修正
    修正

--- app/controllers/umarepos_controller.rb
+++ app/controllers/umarepos_controller.rb
@@ -1,37 +1,21 @@
 class UmareposController < ApplicationController
-
-
   def new
     @umarepos = Umarepo.new
     @cook = Cook.find(params[:cook_id])
-   
-    
   end
 
   def create
-      @umarepos = Umarepo.new(umarepos_params)
-     
-      
+    cook = Cook.find(params[:cook_id])
+    @umarepo = cook.Umarepos.build(umarepos_params)  
     if
-     @umarepos.save!
+      @umarepos.save!
       redirect_to root_path
     else
       render :new
     end
-
   end
 
-  
-
-
-   def umarepos_params
+  def umarepos_params
     params.require(:umarepo).permit(:title,:curator,:comment,:cook_id,  images: [])
   end
-  
-
-
-
-
-
-
 end
