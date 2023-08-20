cb30c981112a327fbdd5f363a8e3aad63364fb38
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-08-20 09:13:45 +0900

    devise update
    devise update

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -1,7 +1,6 @@
 class Cook < ApplicationRecord
   has_many_attached :images
   has_many :umarepos
-
   def self.ransackable_attributes(auth_object = nil)
     ["store_cont"]
   end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,5 @@
 class Place < ApplicationRecord
+   def self.ransackable_attributes(auth_object = nil)
+    ["store_cont","name_eq"]
+   end
 end
