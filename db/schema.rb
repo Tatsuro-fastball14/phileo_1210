85357327c2cc8fab82afb8b0727d857a54c97fbd
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-10-01 08:17:51 +0900

    migration成功
    migration成功

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_02_002915) do
+ActiveRecord::Schema.define(version: 2023_09_30_214853) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
