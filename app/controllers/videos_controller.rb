060ec4586cf82728912ebaff3f7467e9c901556e
yusukexyusuke <yusukexyusuke@gmail.com>
2023-10-10 11:54:53 +0900

    videoを削除できるように

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,26 +1,25 @@
- def index
+class VideosController < ApplicationController
+  def index
     @videos=  Video.all
     render json: @cooks
   end
 
+  def show
+    @video = Video.find(params[:id])
+  end
 
-def show
-  @video = Video.find(params[:id])
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  #destroy actionしているが、binding.pryがかからない
+  def destroy
+    @cook = Cook.find(params[:id])
+    @cook.delete_videos
+    redirect_to cook_path(@cook)
+  end
+
+  def videos_params
+    params.require(:video).permit(images:[],videos:[])
+  end
 end
-
-def edit
-  @video = Video.find(params[:id])
-end
-#destroy actionしているが、binding.pryがかからない
- def destroy
-  @video = Video.find(params[:id])
-  biding.pry
-  @video.destroy
-  redirect_to videos_path
-end
-
-
-def videos_params
-  params.require(:video).permit(images:[],videos:[])
-end
-
