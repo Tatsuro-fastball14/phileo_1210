a50ac6eb6ab217954704caf5e520a881ff8c7c4a
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-10-11 06:12:52 +0900

    transaction
    transaction

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,26 +1,26 @@
- def index
+ class VideosController < ApplicationController
+  def index
     @videos=  Video.all
     render json: @cooks
   end
 
 
-def show
-  @video = Video.find(params[:id])
-end
+  def show
+    @video = Video.find(params[:id])
+  end
 
-def edit
-  @video = Video.find(params[:id])
-end
-#destroy actionしているが、binding.pryがかからない
- def destroy
-  @video = Video.find(params[:id])
-  biding.pry
-  @video.destroy
-  redirect_to videos_path
-end
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def destroy
+    @cook = Cook.find(params[:id])
+    @cook.delete_videos
+    redirect_to cooks_path(@cook)
+  end
 
 
-def videos_params
-  params.require(:video).permit(images:[],videos:[])
+  def videos_params
+    params.require(:video).permit(images:[],videos:[])
+  end
 end
-
--- app/models/cook.rb
+++ app/models/cook.rb
@@ -3,5 +3,10 @@ class Cook < ApplicationRecord
   has_many :umarepos
   has_many_attached :videos
 
- 
+  def delete_videos
+    ActiveRecord::Base.transaction do
+      videos.each { |video| video.purge }
+    end
+  end
+
 end
