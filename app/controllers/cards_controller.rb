9250019201868a4252703e930ec68a2d5e32827d
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-11-16 15:03:44 +0900

    card createをしたい
    card createをしたいが出来ない

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,13 +3,13 @@ class CardsController < ApplicationController
   before_action :set_card
 
   def new      
-  card = Card.where(user_id: current_user.id)
-  if card.exists?
-    redirect_to action: "show"
-  else
-    @card = Card.new(user_id: current_user.id)
+    card = Card.where(user_id: current_user.id)
+    if card.exists?
+      redirect_to action: "show"
+    else
+      @card = Card.new(user_id: current_user.id)
+    end
   end
-end
 
   def show
       card = Card.find_by(user_id: current_user.id)
@@ -23,56 +23,58 @@ end
   end
 
   def destroy   
-  card = Card.find_by(user_id: current_user.id)
-  if card
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    customer = Payjp::Customer.retrieve(card.customer_id)
-    customer.delete_card(card.card_id)
-    card.delete
-    redirect_to root_path, notice: 'カード情報を削除しました。'
-  else
-    redirect_to root_path, alert: 'カード情報が見つかりませんでした。'
+      card = Card.find_by(user_id: current_user.id)
+    if card
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete_card(card.card_id)
+      card.delete
+      redirect_to root_path, notice: 'カード情報を削除しました。'
+    else
+      redirect_to root_path, alert: 'カード情報が見つかりませんでした。'
+    end
   end
-end
 
 
   def create
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
 
-  # トークンが存在しない、または空である場合は処理を中断
-  if params['payjp-token'].blank?
-    binding.pry
-    redirect_to new_card_path, alert: "カード情報を取得できませんでした。"
+      # トークンが存在しない、または空である場合は処理を中断
+    if params['payjp-token'].blank?
+     
+      redirect_to new_card_path, alert: "カード情報を取得できませんでした。"
     return
-  end
+    end
 
-  # PAY.JPに顧客情報を登録し、エラーハンドリングを行う
-  begin
+    # PAY.JPに顧客情報を登録し、エラーハンドリングを行う
+    begin
     customer = Payjp::Customer.create(
       email: current_user.email,
       card: params['payjp-token'],
       metadata: { user_id: current_user.id }
     )
-  rescue Payjp::PayjpError => e
+    rescue Payjp::PayjpError => e
     # PayJPからのエラー応答を処理
-    flash[:alert] = "カード情報の登録に失敗しました。エラー: #{e.message}"
-    redirect_to new_card_path
+      flash[:alert] = "カード情報の登録に失敗しました。エラー: #{e.message}"
+      redirect_to new_card_path
     return
+    
+    end
   end
 
   # アプリケーションのデータベースにカード情報を保存
-  @card = Card.new(
-    user_id: current_user.id,
-    customer_id: customer.id,
-    card_id: customer.default_card
-  )
+  # @card = Card.new(
+  #   user_id: current_user.id,
+  #   customer_id: customer.id,
+  #   card_id: customer.default_card
+  # )
 
   if @card.save
     redirect_to action: "show", notice: 'カード情報を登録しました。'
   else
     redirect_to action: "new", alert: 'カード情報の保存に失敗しました。'
   end
-end
+
 
 
   private
