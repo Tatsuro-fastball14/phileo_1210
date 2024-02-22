aa8838869b260560ef0cd1ded962855e9dca3015
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-02-22 22:29:57 +0900

    修正
    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,32 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:mypage]
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @user = User.update(user_params) # 新しいユーザーインスタンスを作成
+    if @user.save # ユーザーをデータベースに保存
+      flash[:success] = "User was successfully created."
+      redirect_to @user # 例: ユーザーの詳細ページへリダイレクト
+    else
+      render 'edit' # 保存に失敗した場合、編集フォームを再表示
+    end
+  end
 
   def show
      @user = current_user
    
   end
 
-  def mypage
-    redirect_to user_path(current_user)
-  end
-
   def edit
-   @post = User.find(params[:id])
+   @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update(post_params)
+    if @user.update(user_params)
       redirect_to request.referer
     else
-     redirect_to user_path(@user)
+     render 'edit'
     end
   end
 
@@ -40,9 +46,22 @@ class UsersController < ApplicationController
 
   private
 
+  private
+  # user_paramsメソッドを定義
+  def user_params
+    params.require(:user).permit(:name, :profile)
+  end
+
+
+  private
   def set_user
     @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "User not found."
+    redirect_to users_path
   end
+
+
   def after_update_path_for(resource)
     # 自分で設定した「マイページ」へのパス
     users_profile_path
