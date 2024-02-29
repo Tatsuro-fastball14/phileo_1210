2c447b846a179dd09adcd0b0334a810ae1009b2f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-02-29 15:32:39 +0900

    修正
    修正

--- app/models/umarepo.rb
+++ app/models/umarepo.rb
@@ -1,6 +1,7 @@
 class Umarepo < ApplicationRecord
   has_many_attached :images
   belongs_to  :cooks,optional: true
+  has_many_attached :favorites
 
   def favorite?(user)
     favorites.where(user: user).exists?
