c233a083b40cb57575bc25e2017d2dbd07d27657
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-03-23 07:05:09 +0900

    修正
    修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,8 +4,12 @@ class FavoritesController < ApplicationController
   
   def create
     @umarepo_favorite = Favorite.new(user_id: current_user.id, umarepo_id:  params[:umarepo_id])
-    @umarepo_favorite.save
-    redirect_to umarepo_path(params[:umarepo_id]) 
+    if @umarepo_favorite.save
+      update_user_rank(params[:umarepo_id])
+      redirect_to umarepo_path(params[:umarepo_id]), notice: 'いいねを登録しました'
+    else
+      redirect_to umarepo_path(params[:umarepo_id]), alert: 'いいねの登録に失敗しました'
+    end
   end
   
   def destroy
@@ -18,5 +22,14 @@ class FavoritesController < ApplicationController
 
   def favorite_params
     params.require(:favorite).permit(:user_id,:umarepo_id)
+  end
+
+  private
+
+  def update_user_rank(umarepo_id)
+    umarepo = Umarepo.find(umarepo_id)
+    user = umarepo.user
+    total_likes = user.umarepos.joins(:favorites).count
+    user.update_rank(total_likes)
   end
 end
