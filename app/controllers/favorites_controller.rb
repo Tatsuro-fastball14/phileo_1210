1702cd420fcfbeb6d3e529396c973b5e3a0c1e08
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-03-29 07:16:10 +0900

    def update
    def update

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,7 @@ class FavoritesController < ApplicationController
   def create
     @umarepo_favorite = Favorite.new(user_id: current_user.id, umarepo_id:  params[:umarepo_id])
     if @umarepo_favorite.save
-      update_rank(params[:umarepo_id])
+      update(params[:umarepo_id])
       redirect_to users_show_path(params[:umarepo_id]), notice: 'いいねを登録しました'
     else
       redirect_to user_show_path(params[:umarepo_id]), alert: 'いいねの登録に失敗しました'
@@ -18,16 +18,8 @@ class FavoritesController < ApplicationController
     redirect_to umarepo_path(params[:umarepo_id]) 
   end
 
-  private
-
-  def favorite_params
-    params.require(:favorite).permit(:user_id,:umarepo_id)
-  end
-
-  private
-
- def update_rank(total_likes)
-  new_rank = case total_likes
+  def update
+     new_rank = case total_likes
              when 2..Float::INFINITY # 2回以上のいいねでダイヤモンドランクへ
                'diamond'
              when 1 # 1回のいいねでゴールドランクへ
@@ -35,10 +27,11 @@ class FavoritesController < ApplicationController
              else # 0回のいいねはノーマルランク
                'normal'
              end
+  end
 
-  update_rank(rank: new_rank)
-end
+  private
 
-
-
+  def favorite_params
+    params.require(:favorite).permit(:user_id,:umarepo_id)
+  end
 end
