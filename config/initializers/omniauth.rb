b063ddb6edf9af5f02933990ff46c58ee484bfcd
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-06-02 17:03:18 +0900

    修正
    修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,9 +2,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 
-  # on_failure do |env|
-  #   Users::OmniauthCallbacksController.action(:failure).call(env)
-  # end
+  on_failure do |env|
+    Users::OmniauthCallbacksController.action(:failure).call(env)
+  end
 
   OmniAuth.config.allowed_request_methods = [:post, :get]
   OmniAuth.config.silence_get_warning = true
