04f0acc92f43fcd0310b8b2cefea26779ddde12c
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-06-13 07:31:49 +0900

    修正
    修正

--- spec/cook_spec.rb
+++ spec/cook_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Cook, type: :model do
   let(:user) { create(:user) }
   let(:cook) { create(:cook) }
   
+
+   def favorite?(user)
+    favorites.where(user: user).exists?
+   end
 end
 
 
