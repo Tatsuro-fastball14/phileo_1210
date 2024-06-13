365c80686c0f88b6a82bdb6b0187fb359fc24418
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-06-14 07:02:39 +0900

    修正
    修正

--- spec/cook_spec.rb
+++ spec/cook_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Cook, type: :model do
   let(:user) { create(:user) }
   let(:cook) { create(:cook) }
   
+  context 'ユーザーがその料理人をお気に入りに追加していない場合' do
+    it 'falseを返す' do
+      expect(cook.favorite?(user)).to be_falsey
+    end
+  end
 
    def favorite?(user)
     favorites.where(user: user).exists?
