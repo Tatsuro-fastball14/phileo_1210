d2bd909bcf18a3d37428b7ebcb0917f631eea9df
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-06-17 07:11:59 +0900

    修正
    修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,9 +3,4 @@ FactoryBot.define do
     email { "test@example.com" }
     password { "password" }
   end
-
-  it "複数のユーザー登録" do
-    user1 = FactoryBot.create(:user)
-    user2 = FactoryBot.create(:user)
-  end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,9 +4,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "複数のユーザー登録" do
     user1 = FactoryBot.create(:user)
-   
-
-    expect(user1).to be_valid
-    expect(user2).to be_valid
+    user2 = FactoryBot.create(:user)
   end
 end
