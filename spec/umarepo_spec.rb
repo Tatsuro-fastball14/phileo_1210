4e507a8aed9cb818a6d43c2001049070be8726fb
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2024-06-19 06:51:55 +0900

    追加してる場合はtrueを返すテスト
    追加してる場合はtrueを返すテスト

--- spec/umarepo_spec.rb
+++ spec/umarepo_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Umarepo, type: :model do
       expect(umarepo.favorite?(user)).to be_falsey
     end
   end
+
+  context 'ユーザーがその料理人をお気に入りに追加している場合' do
+    before do
+      # ユーザーがumarepoをお気に入りに追加する処理
+      user.favorites.create(umarepo: umarepo)
+    end
+
+    it 'trueを返す' do
+      expect(umarepo.favorite?(user)).to be_truthy
+    end
+  end
 end
