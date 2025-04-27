16cb85d3a6de5bc978437373f040d38b4e5ddea9
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-04-27 12:46:26 +0900

    devise_for :users, controllers: {   registrations: 'users/registrations' }
    devise_for :users, controllers: {
      registrations: 'users/registrations'
    }

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
 
   #  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
    
+  devise_for :users, controllers: {
+  registrations: 'users/registrations'
+}
   
 
   root to: "places#index"
