ab896800ff4a8ee6c0228e7805f3a4345aab9e1a
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-05-05 08:38:51 +0900

    Delete def after_inactive_sign_up_path_for(resource)     super(resource)   end
    Delete def after_inactive_sign_up_path_for(resource)
        super(resource)
      end

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -69,8 +69,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super(resource)
   end
 
-  # The path used after sign up for inactive accounts.
-  def after_inactive_sign_up_path_for(resource)
-    super(resource)
-  end
 end
