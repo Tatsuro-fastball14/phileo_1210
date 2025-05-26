801ad6d53161b75b539590481b40b48f49668a1f
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-05-26 18:42:29 +0900

    plan: 'getugaku
     plan: 'getugaku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -66,7 +66,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
  def after_sign_up_path_for(resource)
-    binding.pry
+    # binding.pry
     
     if  resource.subscriber?
      
