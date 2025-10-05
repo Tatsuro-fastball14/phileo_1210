a439fc0b725dc221c428f68b97112c2cad14cf61
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2025-10-05 14:02:20 +0900

    Fix routes: enable /cards index & cleanup duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,66 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # devise_for :users
   devise_for :admins
+
   get 'places/index'
   get 'cooks/search'
   get 'members/new'
   post 'orders/index'
   post 'orders/pay'
-  post  'orders/destroy'
+  post 'orders/destroy'
   get  'orders/kiyaku'
   get  'umarepos/new'
-  post  'cooks/new'
-  get 'users/mypage'
-  get 'users/show'
-  get 'users/edit'
-  get 'cooks/show'
-  get 'cards/show'
-  get "/test-legal", to: "static_pages#test_legal"
-  get "/privacy", to: "static_pages#privacy"
+  post 'cooks/new'
+  get  'users/mypage'
+  get  'users/show'
+  get  'users/edit'
+  get  'cooks/show'
+  # ▼ 重複・競合の元になるので削除（resources :cards で賄う）
+  # get 'cards/show'
 
-  #  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-   
+  get "/test-legal", to: "static_pages#test_legal"
+  get "/privacy",    to: "static_pages#privacy"
+
   devise_for :users, controllers: {
-  registrations: 'users/registrations'
-}
-  
+    registrations: 'users/registrations'
+  }
 
   root to: "places#index"
- 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cooks  do
+
+  resources :cooks do
     resources :umarepos do
       resources :favorites
     end
   end
 
-  resources :users, only: [:show,:edit, :update, :destroy] do
-    get 'mypage', on: :collection # ユーザーのマイページへのルーティング
-  end
-
-  resources :orders, only: [:show, :create, :index,:destroy] do
-    collection do 
-      get 'order'
-      get  'pay'
-      
+  # users の定義を一つに集約（mypage は collection、destroy_account は member）
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    collection do
+      get :mypage
     end
-  end
-  #ここでルーティング設定している
-  resources :videos
-  resources :cards
-  
-  resources :users do
     member do
       delete :destroy_account
     end
   end
-  
-   if Rails.env.development?
-  mount LetterOpenerWeb::Engine, at: "/letter_opener"
-end
 
-end
+  resources :orders, only: [:show, :create, :index, :destroy] do
+    collection do
+      get :order
+      get :pay
+    end
+  end
 
+  # ▼ カード（解約導線のため /cards を有効化）
+  #   index: /cards （カードあり→show、なければnew へリダイレクト）
+  #   new   : /cards/new
+  #   create: /cards
+  #   show  : /cards/:id
+  #   destroy: /cards/:id
+  resources :cards, only: [:index, :new, :create, :show, :destroy]
+
+  resources :videos
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+end
